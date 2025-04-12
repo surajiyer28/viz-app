8003af6e18f71fef8831ec54d667010c2b5b87e0
suraiyer <suraiyer@iu.edu>
2025-04-12 18:31:44 -0400

    first-commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  title = signal("Viz-App: Language Agnostic Visualization Web Application")
+}
