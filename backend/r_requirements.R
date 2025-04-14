c2af76b0eb6df136dce2c9966898ba4362809b47
suraiyer <suraiyer@iu.edu>
2025-04-14 19:14:53 -0400

    fixed a small bug the visualization display

--- backend/r_requirements.R
+++ backend/r_requirements.R
@@ -1,6 +1,3 @@
 if (!require("ggplot2")) install.packages("ggplot2")
-if (!require("rgl")) {
-  install.packages("rgl", repos="https://cloud.r-project.org")
-}
 if (!require("plotly")) install.packages("plotly")
 if (!require("htmlwidgets")) install.packages("htmlwidgets")
